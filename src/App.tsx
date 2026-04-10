f03d7d162ed4a8069851890a58dfe2f2af0df179
Bryan Wang <v-bryanwang@microsoft.com>
2026-04-10 17:37:14 +0800

    feat:add AppBar and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
+import { Container } from '@mui/material';
 import ResponsiveAppBar from './components/AppBar'
-
+import { grey } from '@mui/material/colors';
 function App() {
 
   return (
-    <>
+    <Container maxWidth={false} disableGutters sx={{width:'100%', minHeight:'100vh'}}>
       <ResponsiveAppBar/>
-    </>
+    </Container>
   )
 }
 
